19ac211aa08a4fa7da32684b1722ba2e1dad2e0c
Denis Denisov <prostolink200@gmail.com>
2021-10-10 21:57:41 +0300

    --new design --moved to .NET framework

---
+++ TravelAgency2018/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TravelAgency2018
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    { 
+        private const string Slogan = "Путешествуй\r\t по России";
+        private static Frame _frame;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SloganTextBlock.Text = Slogan;
+            _frame = MainFrame;
+            MainFrame.Navigate(new Pages.AuthorizationPage());
+        }
+
+        public static void NavigateTo(Page page)
+        {
+            if (page != null) _frame.Navigate(page);
+        }
+    }
+}
+    
---
+++ TravelAgency2018/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TravelAgency2018.Pages
+{
+    public partial class AuthorizationPage : Page
+    {
+        private Random _random = new Random();
+
+        private readonly char[] _captchaDictionary = new char[52]
+        {
+            'A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H',
+            'I', 'G', 'K', 'L',
+            'M', 'N', 'O', 'P',
+            'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd',
+            'e', 'f', 'g', 'h',
+            'i', 'g', 'k', 'l',
+            'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z'
+        };
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+            GetCaptcha();
+        }
+
+        private void GetCaptcha()
+        {
+            int rndCharType;
+            CaptchaTextBlock.Text = string.Empty;
+            FirstCaptchaLine.X1 = _random.Next(0, 61);
+            FirstCaptchaLine.X2 = _random.Next(100, 251);
+            FirstCaptchaLine.Y1 = _random.Next(30, 71);
+            FirstCaptchaLine.Y2 = _random.Next(0, 21);
+            
+            SecondCaptchaLine.X1 = _random.Next(0, 61);
+            SecondCaptchaLine.X2 = _random.Next(100, 251);
+            SecondCaptchaLine.Y1 = _random.Next(0, 21);
+            SecondCaptchaLine.Y2 = _random.Next(30, 71);
+
+            for (int i = 0; i < 11; i++)
+            {
+                rndCharType = _random.Next(0, 2);
+                if (rndCharType == 0)
+                    CaptchaTextBlock.Text += _random.Next(0, 10);
+                
+                else
+                    CaptchaTextBlock.Text += _captchaDictionary[_random.Next(0, 52)];
+            }
+        }
+
+        private void ReloadCaptchaButtonOnClick(object sender, RoutedEventArgs e) => GetCaptcha();
+
+        private void LoginButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            if (CaptchaInputTextBox.Text == CaptchaTextBlock.Text)
+            {
+                string UserLoginDB = "";
+                string UserPasswordDB = "";
+                
+                if (UserInputLoginTextBox.Text == "" && UserInputPasswordBox.Password == "")
+                {
+                    
+                }
+                else
+                { 
+                    MessageBox.Show("Не верный логин или пароль!", "Ошибка", 
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Проверка на робота не пройдена!", "Ошибка", 
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
